60fdcdd7250b3969770cbc428b2115d4e6334629
gongyi.zmp <gongyi.zmp@taobao.com>
2017-11-21 16:03:51 +0800

    statisticsMessages. retry topic support n queue

--- broker/src/main/java/org/apache/rocketmq/broker/processor/StatisticsMessagesProcessor.java
+++ broker/src/main/java/org/apache/rocketmq/broker/processor/StatisticsMessagesProcessor.java
@@ -73,10 +73,12 @@ public class StatisticsMessagesProcessor implements NettyRequestProcessor {
 
         getDelayMessages(topicName, consumerGroup, result);
         getMessages(topicName, consumerGroup, topicConfig.getReadQueueNums(), result);
-		TopicConfig retryTopicConfig = this.brokerController.getTopicConfigManager().selectTopicConfig(KeyBuilder.buildPopRetryTopic(requestHeader.getTopic(), requestHeader.getConsumerGroup()));
-		if (retryTopicConfig != null) {
-	        getMessages(KeyBuilder.buildPopRetryTopic(topicName, consumerGroup), consumerGroup, retryTopicConfig.getReadQueueNums(), result);
-		}
+
+        String retryTopicName = KeyBuilder.buildPopRetryTopic(topicName, consumerGroup);
+        TopicConfig retryTopicConfig = this.brokerController.getTopicConfigManager().selectTopicConfig(retryTopicName);
+        if (retryTopicConfig != null) {
+            getMessages(retryTopicName, consumerGroup, retryTopicConfig.getReadQueueNums(), result);
+        }
         response.setCode(ResponseCode.SUCCESS);
         response.setRemark(null);
         response.setBody(result.encode());
