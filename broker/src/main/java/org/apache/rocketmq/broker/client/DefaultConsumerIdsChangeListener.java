20a2c8fd95575d893b362931adb49b361f1ba825
傅冲 <yubao.fyb@alibaba-inc.com>
2019-01-10 16:57:53 +0800

    fix #18285131

--- broker/src/main/java/org/apache/rocketmq/broker/client/DefaultConsumerIdsChangeListener.java
+++ broker/src/main/java/org/apache/rocketmq/broker/client/DefaultConsumerIdsChangeListener.java
@@ -50,7 +50,7 @@ public class DefaultConsumerIdsChangeListener implements ConsumerIdsChangeListen
             @Override
             public void run() {
                 try {
-                    doNotifyConsumerChange();
+                    notifyConsumerChange();
                 } catch (Exception e) {
                     logger.error("schedule doConsumerChange ", e);
                 }
@@ -94,7 +94,7 @@ public class DefaultConsumerIdsChangeListener implements ConsumerIdsChangeListen
         }
     }
 
-    private void doNotifyConsumerChange() {
+    private void notifyConsumerChange() {
 
         if (consumerChannelMap.isEmpty()) {
             return;
